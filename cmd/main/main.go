cd9538edb09f7aa1c764cb4fa519ccbdb492465d
Celine <celinejams2@gmail.com>
2023-04-05 15:35:31 -0700

    replace pathing to u21-public

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/mattthaber/terraform-bulk-analyzer/internal/PlanAnalyzer"
+	"github.com/u21-public/terraform-plan-analyzer/internal/PlanAnalyzer"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
