7788c9694d45d262b9c9f43fbda79bf27f7ec1ec
Celine <celinejams2@gmail.com>
2023-04-05 16:34:06 -0700

    format

--- internal/PlanAnalyzer/Utils_test.go
+++ internal/PlanAnalyzer/Utils_test.go
@@ -1,8 +1,8 @@
 package PlanAnalyzer
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestGetEmojisEmptyReturn(t *testing.T) {
@@ -17,20 +17,20 @@ func TestGetEmojisEmptyReturn(t *testing.T) {
 func TestGetEmojisNotEmptyReturn(t *testing.T) {
 	input := map[string][]string{
 		"Create": []string{"one", "two", "three"},
-		"Update": []string{"one"},
+		"Update": []string{},
 	}
 	result := getEmojis(input)
-	assert.Equal(t, result, ":pencil2::fountain_pen:", "Result should contain pencil and fountain pen")
+	assert.Equal(t, result, ":pencil2:", "Result should contain pencil")
 }
 
 func TestGetGitDiffMatch(t *testing.T) {
 	result, _ := getGitDiff("Create")
-	
+
 	assert.Equal(t, result, "+", "The result of input Create should return +")
 }
 
 func TestGetGitDiffNoMatch(t *testing.T) {
-	_ , exists := getGitDiff("TestNotAMatch")
+	_, exists := getGitDiff("TestNotAMatch")
 
 	assert.Equal(t, exists, false, "The exists boolean should be false because key is not a match")
 }
