368ccb069fd19b16f84701e62e00109374252b29
Celine <celinejams2@gmail.com>
2023-04-18 00:00:35 -0700

    fix lint errors

--- internal/PlanAnalyzer/plan_test.go
+++ internal/PlanAnalyzer/plan_test.go
@@ -3,10 +3,11 @@ package PlanAnalyzer
 import (
 	"errors"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseWorkspaceNameSuccess(t *testing.T) {
@@ -22,20 +23,28 @@ func TestParseWorkspaceNameEmptyStringErr(t *testing.T) {
 // Can't test this case because line 78 can never be reached "if len(planNoPrefix) == 1" in plan.go
 // func TestParseWorkspaceNameNoPrefixErr(t *testing.T) {
 // 	_, err := ParseWorkspaceName("account-us-west-2-prod1.json")
-//     assert.Equal(t, err, errors.New("plan filename must be prefixed with tfplan-"), "Result should error out with: plan filename must be prefixed with tfplan-")
+//     assert.Equal(
+// 		t,
+// 		err,
+// 		errors.New("plan filename must be prefixed with tfplan-"),
+// 		"Result should error out with: plan filename must be prefixed with tfplan-"
+// 	)
 // }
 
 func TestFilePathWalkDirSuccess(t *testing.T) {
-	var expected_files_list []string
-	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+	var expectedFilesList []string
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
-			expected_files_list = append(expected_files_list, path)
+			expectedFilesList = append(expectedFilesList, path)
 		}
 		return nil
 	})
+	if err != nil {
+		fmt.Println("Error with iterating current directory")
+	}
 
 	result, _ := FilePathWalkDir(".")
-	assert.Equal(t, result, expected_files_list, "Result should contain all file names in current directory")
+	assert.Equal(t, result, expectedFilesList, "Result should contain all file names in current directory")
 }
 
 // How to test for error path here? maybe we need better error handling
@@ -51,8 +60,8 @@ func TestFilePathWalkDirSuccess(t *testing.T) {
 // 	// assert.Equal(t, result, expected_files_list, "Result should contain all file names in current directory")
 // }
 
-func TestReadPlansSuccess(t *testing.T) {
-	result := ReadPlans("test_data/tfplan-account-region-environment.json")
-	fmt.Println(result[0])
-	fmt.Println([]PlanExtended(result))
-}
+// func TestReadPlansSuccess(t *testing.T) {
+// 	result := ReadPlans("test_data/tfplan-account-region-environment.json")
+// 	fmt.Println(result[0])
+// 	fmt.Println([]PlanExtended(result))
+// }
